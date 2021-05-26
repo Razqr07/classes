5c808ca8254445b9a92622f00839a0f71fd4d08c
Razqr07 <71629000+Razqr07@users.noreply.github.com>
2021-05-26 11:50:42 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Standaard = new Student();
+            Console.WriteLine("The students name is " + Standaard.Name + " And the year of birth is " + Standaard.YearOfBirth + 
+                " their hobbies are " + Standaard.Hobby + " and is he alive " + Standaard.Alive  );
+            Student Archibald = new Student("Archibald", 2002, "Fappen", true);
+            Console.WriteLine("The students name is " + Archibald.Name + " And the year of birth is " + Archibald.YearOfBirth +
+                " their hobbies are " + Archibald.Hobby + " and is he alive " + Archibald.Alive);
+
+        }
+    }
+}
