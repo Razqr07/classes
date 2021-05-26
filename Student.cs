ee1f9d5d13c40a513ead3299982638d73bb11704
Razqr07 <71629000+Razqr07@users.noreply.github.com>
2021-05-26 11:51:29 +0200

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Classes
+{
+    class Student
+    {
+        public string Name { get; }
+        public int YearOfBirth { get; }
+        public string Hobby {get; set; }
+        public bool Alive {get; set;}
+
+        public Student()
+        {
+            Name = "John";
+            YearOfBirth = 2000;
+            Hobby = "Nothing";
+            Alive = true;
+        }
+
+
+        public Student(string name, int yearOfBirth, string hobby, bool alive)
+        {
+            Name = name;
+            YearOfBirth = yearOfBirth;
+            Hobby = hobby;
+            Alive = alive;
+
+        }
+
+        string IsAlive()
+        {
+            if (Alive == true)
+            {
+                return "Alive";
+            }
+            else
+            {
+                return "not Alive";
+            }
+        }
+    }
+}
